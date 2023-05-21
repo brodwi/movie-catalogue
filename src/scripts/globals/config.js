1256520b907912bb5421e08bfd32f224deb24329
brodwi <121082176+brodwi@users.noreply.github.com>
2023-05-21 13:26:49 +0700

    initial commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: '1f5ac2cd227b59e0dfbba5dcee127e77',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+  };
+   
+  export default CONFIG;
