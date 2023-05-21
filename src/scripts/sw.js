472e87f7a247b7969c65f47f8f1df21ee9e6652b
brodwi <121082176+brodwi@users.noreply.github.com>
2023-05-21 14:06:14 +0700

    Cache API Dalam Service Worker

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -18,15 +18,15 @@ const assetsToCache = [
   './app.webmanifest',
   './sw.bundle.js',
 ];
-
+ 
 self.addEventListener('install', (event) => {
-    event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
-  });
-   
-  self.addEventListener('activate', (event) => {
-    event.waitUntil(CacheHelper.deleteOldCache());
-  });
-   
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(CacheHelper.revalidateCache(event.request));
-  });
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
+});
+ 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(CacheHelper.deleteOldCache());
+});
+ 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(CacheHelper.revalidateCache(event.request));
+});
